d91f292f45860f5f041b28cbed9ac8e50be05f65
daniele <dpadula85@yahoo.it>
2015-11-25 19:41:28 +0100

    Corrected a line in helix.py

--- helix.py
+++ helix.py
@@ -236,7 +236,7 @@ if __name__ == '__main__':
     # final = final[final[:,2].argsort()]
     final_save = np.c_[np.ones(len(final)), final]
     u.write_XYZ('%s_S%d%s_B%d%s.xyz' % (args.output, ps, hs, pb, hb), final_save)
-    print("Output saved in %s_S%d%s_B%d%s" % (args.output, ps, hs, pb, hb))
+    print("Output saved in %s_S%d%s_B%d%s.xyz" % (args.output, ps, hs, pb, hb))
 
     print
     print u.banner(ch='=', length=80)
