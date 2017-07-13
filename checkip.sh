016b5742050aad64b6ac88074049ca0c6599399e
Daniele Padula <dpadula85@yahoo.it>
2017-07-13 21:05:42 +0100

    ip check

--- checkip.sh
+++ checkip.sh
@@ -2,7 +2,8 @@
 
 ip=`curl ifconfig.me 2> /dev/null`
 oldip=`grep -A 1 " pi" ~/.ssh/config | tail -1 | awk '{print $2}'`
+line_no=`grep -nA 1 " pi" ~/.ssh/config | tail -1 | cut -d "-" -f1`
 
 if [ "$ip" != "$oldip" ]; then
-  sed -i "s/HostName.*$/HostName $ip/" ~/.ssh/config
+  sed -i "${line_no}s/HostName.*$/HostName $ip/" ~/.ssh/config
 fi
