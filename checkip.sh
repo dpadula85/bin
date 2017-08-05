7bc21eed914afecc6c77c6c78406293aa56c3e02
Daniele Padula <dpadula85@yahoo.it>
2017-08-06 00:13:28 +0100

    modified ip check

--- checkip.sh
+++ checkip.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-ip=`curl ifconfig.me 2> /dev/null`
-oldip=`grep -A 1 " pi" ~/.ssh/config | tail -1 | awk '{print $2}'`
-line_no=`grep -nA 1 " pi" ~/.ssh/config | tail -1 | cut -d "-" -f1`
+essid=`nmcli dev wifi | grep "*" | tail -n 1 | awk '{print $2}'`
 
-if [ "$ip" != "$oldip" ]; then
-  sed -i "${line_no}s/HostName.*$/HostName $ip/" ~/.ssh/config
+if [ "$essid" = "Obi" ]; then
+
+    ip=`curl ifconfig.me 2> /dev/null`
+    oldip=`grep -A 1 " pi" ~/.ssh/config | tail -1 | awk '{print $2}'`
+    line_no=`grep -nA 1 " pi" ~/.ssh/config | tail -1 | cut -d "-" -f1`
+    
+    if [ "$ip" = "" ]; then
+        exit
+    fi
+    
+    if [ "$ip" != "$oldip" ]; then
+      sed -i "${line_no}s/HostName.*$/HostName $ip/" ~/.ssh/config
+    fi
+
 fi
