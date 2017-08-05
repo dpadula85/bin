d4b9536cd1127c76f56a0039e79334c225caeb0b
Daniele Padula <dpadula85@yahoo.it>
2017-08-06 00:28:00 +0100

    improved ip check

--- checkip.sh
+++ checkip.sh
@@ -1,19 +1,29 @@
 #!/bin/sh
 
+#
+# Get Raspi IP from internet if connected to the same network or from email if not
+#
 essid=`iwgetid -r`
-
 if [ "$essid" = "Obi WAN" ]; then
 
     ip=`curl ifconfig.me 2> /dev/null`
-    oldip=`grep -A 1 " pi" ~/.ssh/config | tail -1 | awk '{print $2}'`
-    line_no=`grep -nA 1 " pi" ~/.ssh/config | tail -1 | cut -d "-" -f1`
-    
-    if [ "$ip" = "" ]; then
-        exit
-    fi
-    
-    if [ "$ip" != "$oldip" ]; then
-      sed -i "${line_no}s/HostName.*$/HostName $ip/" ~/.ssh/config
-    fi
 
+else
+
+    ip=`grep "Public IP" "$HOME/.thunderbird/e2l0inr0.default/Mail/Local Folders/Inbox.sbd/Raspi" | tail -n 1 | awk '{print $NF}'`
+
+fi
+
+#
+# Replace old IP with new one
+#
+oldip=`grep -A 1 " pi" ~/.ssh/config | tail -1 | awk '{print $2}'`
+line_no=`grep -nA 1 " pi" ~/.ssh/config | tail -1 | cut -d "-" -f1`
+
+if [ "$ip" = "" ]; then
+    exit
+fi
+
+if [ "$ip" != "$oldip" ]; then
+  sed -i "${line_no}s/HostName.*$/HostName $ip/" ~/.ssh/config
 fi
